6658723c15a04e535bcaa5bf4df034c657675733
Deepak <deepkhanowal96@gmail.com>
2023-01-11 18:14:31 +0530

    info folder

---
+++ info/app.js
@@ -0,0 +1,6 @@
+//step 1 : Connect to the rabbitmq server
+//step 2 : Create a new channel
+//step 3 : Create the exchange
+//step 4 : Create the queue
+//step 5 : Bind the queue to the exchange
+//step 6 : Consume messages from the queue
